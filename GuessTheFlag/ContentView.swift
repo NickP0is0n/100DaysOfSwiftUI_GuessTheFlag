0722673951ddee2e8525d1eb9144713d2cead550
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-24 14:50:04 +0300

    Stacks and spacers

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            Text("Hello World")
+            Text("Another View")
+            Text("Third")
+            
+            Spacer()
+            Spacer()
         }
-        .padding()
     }
 }
 
