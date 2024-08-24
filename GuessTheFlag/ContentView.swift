f422d842f6a380186213f766f10873f3cdac9fce
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-24 15:20:17 +0300

    Colors and materials

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,29 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
+            Color.purple
             VStack {
-                HStack {
-                    Text("1")
-                    Text("2")
-                    Text("3")
+                ZStack {
+                    VStack(spacing: 0) {
+                        Color.blue
+                        Color.yellow
+                    }
+                    Text("My content")
+                        .padding(50)
+                        .foregroundStyle(.secondary)
+                        .background(.ultraThinMaterial)
                 }
-                HStack {
-                    Text("4")
-                    Text("5")
-                    Text("6")
+                ZStack {
+                    Color.indigo
+                        .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+                    Text("My content")
                 }
-                HStack {
-                    Text("7")
-                    Text("8")
-                    Text("9")
+                ZStack {
+                    Color.primary
+                        .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
+                    Text("My content")
+                        .background(.secondary)
                 }
-            }
-            ZStack(alignment: .top) {
-                Text("I am at the top")
-                Text("Bottom guy")
             }
         }
+        .ignoresSafeArea()
     }
 }
 
