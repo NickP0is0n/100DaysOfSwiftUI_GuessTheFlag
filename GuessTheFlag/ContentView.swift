23a4c23c1069d2cf7cbebdb6664989153bcb6471
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-24 14:56:15 +0300

    Different stacks

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,13 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Spacer()
-            Text("Hello World")
-            Text("Another View")
-            Text("Third")
-            
-            Spacer()
-            Spacer()
+            VStack {
+                HStack {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+                HStack {
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                }
+                HStack {
+                    Text("7")
+                    Text("8")
+                    Text("9")
+                }
+            }
+            ZStack(alignment: .top) {
+                Text("I am at the top")
+                Text("Bottom guy")
+            }
         }
     }
 }
