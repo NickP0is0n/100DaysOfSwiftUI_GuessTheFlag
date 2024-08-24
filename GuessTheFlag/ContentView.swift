ecd091569a6598ffc203aff24df5b993517535c9
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-24 16:59:40 +0300

    Images

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,24 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Button("Button 1") {}
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) {}
-                .buttonStyle(.bordered)
-            Button("Button 3") {}
-                .buttonStyle(.borderedProminent)
-                .tint(.indigo)
-            Button("Button 4", role: .destructive) {}
-                .buttonStyle(.borderedProminent)
-            
-            Button {
-                print("Button was tapped.")
-            } label: {
-                Text("Tap me!")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.red)
-            }
+            Image(decorative: "catmeme")
+            Image(systemName: "pencil.circle")
         }
     }
 }
