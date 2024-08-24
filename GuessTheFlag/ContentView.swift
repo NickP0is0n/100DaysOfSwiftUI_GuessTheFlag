c84f12d4bc199f5c12b2f6a3e802e3ba978d422a
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-24 16:50:06 +0300

    Buttons

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,33 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.purple
-            VStack {
-                ZStack {
-                    VStack(spacing: 0) {
-                        Color.blue
-                        Color.yellow
-                    }
-                    Text("My content")
-                        .padding(50)
-                        .foregroundStyle(.secondary)
-                        .background(.ultraThinMaterial)
-                }
-                ZStack {
-                    Color.indigo
-                        .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-                    Text("My content")
-                }
-                ZStack {
-                    Color.primary
-                        .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 200)
-                    Text("My content")
-                        .background(.secondary)
-                }
+        VStack {
+            Button("Button 1") {}
+                .buttonStyle(.bordered)
+            Button("Button 2", role: .destructive) {}
+                .buttonStyle(.bordered)
+            Button("Button 3") {}
+                .buttonStyle(.borderedProminent)
+                .tint(.indigo)
+            Button("Button 4", role: .destructive) {}
+                .buttonStyle(.borderedProminent)
+            
+            Button {
+                print("Button was tapped.")
+            } label: {
+                Text("Tap me!")
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.red)
             }
         }
-        .ignoresSafeArea()
     }
 }
 
