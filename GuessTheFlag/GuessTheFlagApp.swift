b57325331a2c9eb948d417f8f02eb38129eb6450
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-24 14:38:18 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Mykola Chaikovskyi on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
